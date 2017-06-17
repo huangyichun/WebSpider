1b2f9986b13cc9320cebc4981507fa72c771a27c
huangyichun <576305972@qq.com>
2017-06-17 15:16:13 +0800

    下载的页面，包括当前页所在的URL和resultItems，targetRequests,helpRequest

---
+++ src/main/java/domain/Page.java
@@ -0,0 +1,65 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 下载的页面
+ */
+public class Page {
+
+    private Request request;
+
+    private ResultItems resultItems = new ResultItems();
+
+    private List<Request> targetRequests = new ArrayList<Request>();
+
+    private List<Request> helpRequest = new ArrayList<>();
+
+    private String html;
+
+    public Page(Request request, String html) {
+        this.request = request;
+        this.html = html;
+    }
+
+    public List<Request> getHelpRequest() {
+        return helpRequest;
+    }
+
+    public void setHelpRequest(List<Request> helpRequest) {
+        this.helpRequest = helpRequest;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public ResultItems getResultItems() {
+        return resultItems;
+    }
+
+    public void setResultItems(ResultItems resultItems) {
+        this.resultItems = resultItems;
+    }
+
+    public List<Request> getTargetRequests() {
+        return targetRequests;
+    }
+
+    public void setTargetRequests(List<Request> targetRequests) {
+        this.targetRequests = targetRequests;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+}
