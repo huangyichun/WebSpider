61609f680eac0f7a2b9169461dab538bc84f0593
huangyichun <576305972@qq.com>
2017-07-01 16:00:06 +0800

    使用新的构造函数，删除了一些无用的变量

--- src/test/java/domain/SpiderTest.java
+++ src/test/java/domain/SpiderTest.java
@@ -1,18 +1,8 @@
 package domain;
 
-import download.Downloader;
-import download.HttpClientGetDownloader;
-import org.junit.Before;
-import org.junit.Test;
-import pipeline.ConsolePipeline;
-import pipeline.Pipeline;
 import processor.InformationPageProcessor;
 import processor.PageProcessor;
-import scheduler.DuplicateRemovedScheduler;
-import scheduler.Scheduler;
 import selector.InformationSelector;
-
-import static org.junit.Assert.*;
 
 /**
  * Spider测试
@@ -20,46 +10,23 @@ import static org.junit.Assert.*;
  */
 public class SpiderTest {
 
-    private Site site;
-    private Request firstRequest;
-    private Downloader downloader;
-    private PageProcessor process;
-    private Pipeline pipeline;
-    private Scheduler scheduler;
-    private String helpUrlPattern;
-    private String targetUrlPattern;
-    private InformationSelector selector;
-
     public static void main(String[] args) {
-        Site site;
-        Request firstRequest;
-        Downloader downloader;
-        PageProcessor process;
-        Pipeline pipeline;
-        Scheduler scheduler;
-        String helpUrlPattern;
-        String targetUrlPattern;
-        InformationSelector selector;
-        site = new Site("http://cs.scu.edu.cn", "GBK");
-        helpUrlPattern = "http://cs.scu.edu.cn/cs/xyxw/H9501index.+?htm";
-        targetUrlPattern = "http://cs.scu.edu.cn/cs/xyxw/webinfo.+?htm";
-        selector = new InformationSelector();
-        selector.setTitleSelect("width=721 height=27 a> <DIV align=center>(.+?)</DIV> <DIV align=center>");
-        selector.setCollegeSelect("valign=\"bottom\">来源： </SPAN>(.+?)<SPAN class=hangjc");
-        selector.setTimeSelect("valign=\"bottom\">时间： </SPAN>(.+?)<SPAN");
-        selector.setPicSeclect("src=\"(/cs/rootimages.+?jpg)\"");
-        selector.setContentSelect("#BodyLabel");
-        firstRequest = new Request("http://cs.scu.edu.cn/cs/xyxw/H9501index_1.htm");
-        downloader = new HttpClientGetDownloader();
-        process = new InformationPageProcessor(site,
+        Site site = new Site("http://cs.scu.edu.cn", "GBK");
+        String helpUrlPattern = "http://cs.scu.edu.cn/cs/xyxw/H9501index.+?htm";
+        String targetUrlPattern = "http://cs.scu.edu.cn/cs/xyxw/webinfo.+?htm";
+
+        InformationSelector selector = new InformationSelector();
+        selector.setTitleSelect("width=721 height=27 a> <DIV align=center>(.+?)</DIV> <DIV align=center>")
+                .setCollegeSelect("valign=\"bottom\">来源： </SPAN>(.+?)<SPAN class=hangjc")
+                .setTimeSelect("valign=\"bottom\">时间： </SPAN>(.+?)<SPAN")
+                .setPicSeclect("src=\"(/cs/rootimages.+?jpg)\"")
+                .setContentSelect("#BodyLabel");
+
+        Request firstRequest = new Request("http://cs.scu.edu.cn/cs/xyxw/H9501index_1.htm");
+        PageProcessor process = new InformationPageProcessor(site,
                 helpUrlPattern, targetUrlPattern, selector);
-        pipeline = new ConsolePipeline();
-        scheduler = new DuplicateRemovedScheduler();
-        Spider spider = new Spider(site, firstRequest, downloader,
-                process, pipeline, scheduler);
 
+        Spider spider = new Spider(site, firstRequest, process);
         spider.run();
-
     }
-
 }
